7058c0c2e156d4e883682b89aba1358298ac8c20
Kritika Kumari <33561960+kritikakriti@users.noreply.github.com>
2020-11-27 00:25:37 +0530

    Update opencv.py

--- data/opencv.py
+++ data/opencv.py
@@ -7,7 +7,25 @@ def loadImages(path_r = "input/"):
 filenames = loadImages()
 images = []
 for file in filenames:
-    images.append(cv2.imread(file, cv2.IMREAD_GRAYSCALE))
+    input_image = cv2.imread(file, cv2.IMREAD_GRAYSCALE))
+    img = cv2.flip(input_image, 1)
+    # blur image to reduce noise
+    img_blur = cv2.medianBlur(img, 5)
+
+    # equalize the histogram to improve contrast
+    img_eq = cv2.equalizeHist(img_blur)
+    # create Contrast Limited Adaptive Histogram Equalization
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+    cl1 = clahe.apply(img_blur)
+
+    # set global threshold value to eliminate grey values (binary)
+    th0 = cv2.adaptiveThreshold(cl1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    ret,tsh2 = cv2.threshold(th0,127,255,cv2.THRESH_BINARY_INV)
+    
+    # median to reduce noise
+    median = cv2.medianBlur(tsh2, 3)
+    # blur to smooth edges
+    blur = cv2.GaussianBlur(median, (3, 3), 0)
 
 num = 0
 for image in images:
