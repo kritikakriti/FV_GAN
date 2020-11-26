c2c1bfc11bbe1a24a7e8539d7439b6c0ce5989f2
Kritika Kumari <33561960+kritikakriti@users.noreply.github.com>
2020-11-26 23:33:31 +0530

    Update opencv.py

--- data/opencv.py
+++ data/opencv.py
@@ -1,15 +1,17 @@
 import cv2
 import os
 
-def loadImages(path = "input/"):
-    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".bmp")]
-
-
+def loadImages(path_r = "input/"):
+    return [os.path.join(path_r, f) for f in os.listdir(path_r) if f.endswith(".bmp")]
 
 filenames = loadImages()
 images = []
 for file in filenames:
     images.append(cv2.imread(file, cv2.IMREAD_GRAYSCALE))
-#cv2.imread()  
+
+num = 0
+for image in images:
+    cv2.imwrite("output/", (str(num)+".png", image))
+    num += 1
   
   
