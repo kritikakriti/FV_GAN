43835c503643fad5d022ce69ea01f3190eb81c3a
Kritika Kumari <33561960+kritikakriti@users.noreply.github.com>
2020-11-27 22:29:55 +0530

    Update opencv.py

--- data/opencv.py
+++ data/opencv.py
@@ -1,5 +1,6 @@
-import cv2
-import os
+import cv2 
+import numpy as np
+from skimage import morphology
 
 def loadImages(path_r = "input/"):
     return [os.path.join(path_r, f) for f in os.listdir(path_r) if f.endswith(".jpg")]
@@ -14,10 +15,13 @@ print(images)
 
 num = 0
 for image in images:
-    input_image = image
-    img = cv2.flip(input_image, 1)
+    img = image
+    
+    #Grayscale normalization
+    norm_img = np.zeros((800,800))
+    final_img = cv2.normalize(img,  norm_img, 0, 255, cv2.NORM_MINMAX)
     # blur image to reduce noise
-    img_blur = cv2.medianBlur(img, 5)
+    img_blur = cv2.medianBlur(final_img, 5)
 
     # equalize the histogram to improve contrast
     img_eq = cv2.equalizeHist(img_blur)
@@ -28,15 +32,30 @@ for image in images:
     # set global threshold value to eliminate grey values (binary)
     th0 = cv2.adaptiveThreshold(cl1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     ret,tsh2 = cv2.threshold(th0,127,255,cv2.THRESH_BINARY_INV)
+    
+    #denoising
+    openkernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    opening = cv2.morphologyEx(tsh2, cv2.MORPH_OPEN, openkernel)
+    closedkernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    closed = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, closedkernel)
+    result = cv2.medianBlur(closed, 5)
 
-    # median to reduce noise
-    median = cv2.medianBlur(tsh2, 3)
-    # blur to smooth edges
-    blur = cv2.GaussianBlur(median, (3, 3), 0)
+    #thining
+    pix = result
+    pix = pix / 255
+    pix = morphology.skeletonize(pix)
+    pix = pix.astype(int) * 255
+    #cv2_imshow(result)
+
+    #clean small objects
+    pix = pix / 255
+    pix = pix.astype(bool)
+    pix = morphology.remove_small_objects(pix, min_size=20, connectivity=2)
+    pix = pix.astype(int) * 255
     
     #output image in output folder
     path = "output/"
-    cv2.imwrite(os.path.join(path, str(num)+".jpg"), blur)
+    cv2.imwrite(os.path.join(path, str(num)+".jpg"), pix)
     num += 1
   
   
